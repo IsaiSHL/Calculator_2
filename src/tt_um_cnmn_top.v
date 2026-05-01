3e945ad6f3e83dec63671500a5b2b32780aee4be
IsaiSHL <hdezisai@ieee.org>
2026-05-01 04:20:54 -0600

    Update tt_um_cnmn_top.v

--- src/tt_um_cnmn_top.v
+++ src/tt_um_cnmn_top.v
@@ -6,7 +6,7 @@
 `default_nettype none
 
 
-module tt_um_shuangyu_top (
+module tt_um_cnmn_top (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
